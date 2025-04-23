0e4299bab0a27cfa7ddbac252ac21f69295924a2
khadev <lekha3604@gmail.com>
2025-04-24 00:29:03 +0700

    update wss for Websocket

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = 'https://note-app-by-lbk.onrender.com';
-export  const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://note-app-by-lbk.onrender.com/graphql';
+export  const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://note-app-by-lbk.onrender.com/graphql';
